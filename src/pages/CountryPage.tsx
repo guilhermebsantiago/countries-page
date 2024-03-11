330c4a15493eb93db38c4a66779428cce6f10bf2
guilhermebsantiago <guilhermebscontact@gmail.com>
2024-03-11 10:59:54 -0300

    feat: dynamic pages fixed error

--- src/pages/CountryPage.tsx
+++ src/pages/CountryPage.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 import { useParams } from 'react-router-dom'
+import { Error } from '../components/Error';
 
 export const CountryPage = () => {
   interface Data {
@@ -12,9 +13,9 @@ export const CountryPage = () => {
     };
     name: {
       common: string;
-      official: string;
+      official?: string;
       nativeName: {
-        div: {
+        eng: {
           official: string;
           common: string;
         };
@@ -31,7 +32,7 @@ export const CountryPage = () => {
     region: string;
     subregion: string;
     languages: {
-      div: string;
+      [key:string]: string;
     };
     borders: string[];
     population: number;
@@ -39,7 +40,7 @@ export const CountryPage = () => {
   
   const {countryName} = useParams()
     
-    const { data, isLoading } = useQuery({
+    const { data, isLoading, error } = useQuery({
       queryKey: [""],
       queryFn: async () => {
         const { data } = await axios.get(
@@ -49,53 +50,61 @@ export const CountryPage = () => {
         return data as Data[];
       },
     });
-  
-     const countryData = data
     
- 
   return (
     <>
+    {error?<Error/>:null}
     {isLoading?
       <div className="h-screen flex justify-center w-full">
     <p className="font-bold">Data is loading...</p>
   </div>:
       <div className='w-full h-full flex justify-center'>
-        <img src={countryData?.flags.svg} alt={countryData?.flags.alt} />
+        <img src={data[0]?.flags.svg} alt={data[0]?.flags.alt} className='w-1/3'/>
         <div>
           <div>
         <p className="font-bold dark:text-white text-very-dark-blue">
-            {countryData?.name.common}
+            {data[0]?.name.common}
           </p>
           <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Native Name:</span> {countryData?.name.nativeName.div.official}
+            <span className="font-semibold">Native Name:</span> {data[0]?.name.nativeName.eng.common}
           </p>
           <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Population:</span> {countryData?.population}
+            <span className="font-semibold">Population:</span> {data[0]?.population}
           </p>
           <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Region:</span> {countryData?.capital}
+            <span className="font-semibold">Region:</span> {data[0]?.capital}
           </p>
           <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Sub Region:</span> {countryData?.subregion}
+            <span className="font-semibold">Sub Region:</span> {data[0]?.subregion}
           </p>
           <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Capital:</span> {countryData?.capital}
+            <span className="font-semibold">Capital:</span> {data[0]?.capital}
           </p>
         </div>
         <div>
         <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Top LEvel Domain:</span> {countryData?.tld}
+            <span className="font-semibold">Top Level Domain:</span> {data[0]?.tld}
           </p>
           <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Currencies:</span> {/* {countryData?.currencies} */}
+            <span className="font-semibold">Currencies:</span> {Object.entries(data[0]?.currencies).map((e,i,arr)=>{
+              if(i == arr.length -1){
+                return e[1].name
+              }
+              return `${e[1].name}, `}
+              )}
           </p>
           <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Languages:</span> {countryData?.languages.div}
+            <span className="font-semibold">Languages:</span> {Object.values(data[0]?.languages).map((e,i,arr)=>{
+               if(i == arr.length -1){
+                return e
+              }
+              return `${e}, `
+            })}
           </p>
         </div>
 
         <p className="dark:text-white text-very-dark-blue">
-            <span className="font-semibold">Border Countries:</span> {countryData?.borders}
+            <span className="font-semibold">Border Countries:</span> {data[0]?.borders}
           </p>
 
         </div>
